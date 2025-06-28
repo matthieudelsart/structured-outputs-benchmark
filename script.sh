5dc7e485e2a425fdf24d59ca3281cd63e0833edb
root <matthieu.delsart@gmail.com>
2025-06-28 22:40:47 +0000

    generate 1b

--- script.sh
+++ script.sh
@@ -4,11 +4,10 @@ set -euo pipefail
 
 # uv run python -m src.generate_transformers --model google/gemma-3-4b-it
 
-# git add .
-# git commit -m "Add results transformers"
-# git push
+export HUGGING_FACE_HUB_TOKEN=XX
 
-uv run python -m src.generate_vllm --model google/gemma-3-12b-it >logs/run_vllm_12b.log 2>&1 
-uv run python -m src.generate_vllm_outlines --model google/gemma-3-12b-it >logs/run_vllm_12b_outlines.log 2>&1 
+
+uv run python -m src.generate_vllm --model google/gemma-3-1b-it >logs/run_vllm_1b.log 2>&1 
+uv run python -m src.generate_vllm_outlines --model google/gemma-3-1b-it >logs/run_vllm_1b_outlines.log 2>&1 
 
 git add .
