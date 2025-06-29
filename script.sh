d3183673c321bf0f1565fa04299dfdfa1e568bca
root <matthieu.delsart@gmail.com>
2025-06-29 11:22:54 +0000

    generate the missing reasoning tasks

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # export HUGGING_FACE_HUB_TOKEN=XX
 
 
-# uv run python -m src.generate_vllm --model google/gemma-3-1b-it >logs/run_vllm_1b.log 2>&1 
-uv run python -m src.generate_vllm_outlines --model google/gemma-3-1b-it --start-from 3-few_nerd >>logs/run_vllm_1b_outlines.log 2>&1 
+uv run python -m src.generate_transformers_outlines --model google/gemma-3-4b-it --start-from 6-reasoning >>logs/run_transformers_4b_outlines.log 2>&1 
+
 
 git add .
